4238d9f00f331a23fa92c37abe7ad67ff7a57b3f
Avtandil Shaburishvili <a.shaburishvili@gmail.com>
2021-07-15 18:59:59 +0400

    Landing Screen

--- MerchantApp/App.tsx
+++ MerchantApp/App.tsx
@@ -1,56 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import {ScrollView, StatusBar,StyleSheet,Text,View, TouchableOpacity} from 'react-native';
-import { getUniqueId, getManufacturer, getDevice, getDeviceName } from 'react-native-device-info';
-import AppInput from './Components/AppInput/AppInput';
-
-
-
+import Dashboard from './Screens/Dashboard/Dashboard';
 
 
  const App = () => {
-  
-  const [ deviceId, setDeviceId ] = useState('');
-  const [ cardBarcode, setCardBarCode ] = useState('');
-  const [ amount, setAmount ] = useState('');
-
-  useEffect(() => {
-    setDeviceId(getUniqueId())
-  }, [])
-  
- 
-  
+   
    return (
     <View style = {{flex: 1}}>
-      <Text> device unique id: {deviceId}</Text>
-      <AppInput label = 'ბარათი' value = {cardBarcode} onChangeText = {(newVal: string) => setCardBarCode(newVal)}/>
-      
-      <AppInput label = 'თანხა' value = {amount} keyboardType = 'numeric' onChangeText = {(newVal: string) => setAmount(newVal)}/>
-      {/* <TouchableOpacity style={styles.sectionContainer}>
-        <Text>დაგროვება/დახარჯვა</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.sectionContainer}>
-        <Text>დღის დახურვა</Text>
-      </TouchableOpacity> */}
-     
+      <Dashboard/>
 
     </View>
    );
  };
 
- const styles = StyleSheet.create({
-   sectionContainer: {
-     borderWidth: 1,
-     borderColor: 'black',
-     borderRadius: 7,
-     width: 300,
-     alignSelf: 'center',
-     display: 'flex',
-     alignItems: 'center',
-     justifyContent: 'center',
-     height: 40,
-     paddingHorizontal: 24,
-   },
- 
- });
+
 
  export default App;
--- MerchantApp/index.js
+++ MerchantApp/index.js
@@ -7,4 +7,4 @@ import App from './App';
 import {name as appName} from './app.json';
 import BarCodeReader from './BarCodeReader';
 
-AppRegistry.registerComponent(appName, () => BarCodeReader);
+AppRegistry.registerComponent(appName, () => App);
