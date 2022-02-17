40ef10c376e932fd072c8290e800d79ef5e46a08
avtandil shaburishvili <a.shaburishvili@gmail.com>
2022-02-17 15:03:03 +0400

    fix2

--- Components/VoucherSuccessModal.tsx
+++ Components/VoucherSuccessModal.tsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        // backgroundColor: '#000000a3',
+        backgroundColor: '#000000a3',
     },
 
     modalView: {
