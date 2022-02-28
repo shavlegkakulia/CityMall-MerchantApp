3675b79caa0dcdd6c23d9292202740d322354e5e
shavleg kakulia <kakuliash@gmail.com>
2022-02-28 20:04:12 +0400

    add prod credentials

--- config/env.ts
+++ config/env.ts
@@ -5,8 +5,8 @@ const devEnvironmentVariables = {
 } 
 
 const prodEnvironmentVariables = {
-    API_URL: 'https://citymallapi.payunicard.ge:8061',
-    CONNECT_URL: 'https://citymallidentity.payunicard.ge:8060',
+    API_URL: 'https://cmapi.payunicard.ge:18011',
+    CONNECT_URL: 'https://cmidentity.payunicard.ge:17411',
     TOKEN_TTL: ''
 }
 
