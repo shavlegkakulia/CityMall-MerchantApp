1c4b2471334aedf422ae217fa87ac55f2786210b
shavleg kakulia <kakuliash@gmail.com>
2022-03-07 17:49:19 +0400

    autoupdate

--- App.tsx
+++ App.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import AppIndex from './AppIndex';
 import AuthProvider from './services/ContextService';
+import codePush from 'react-native-code-push';
 
+let codePushOptions = {checkFrequency: codePush.CheckFrequency.ON_APP_START};
 
 const App = () => {
+
+  useEffect(() => {
+    codePush
+    .sync({
+      updateDialog: {title: 'update?'},
+      installMode: codePush.InstallMode.IMMEDIATE,
+    },() => {},e => console.log(e.totalBytes, e.receivedBytes));
+  }, []);
+
   return (
     <AuthProvider>
-      <AppIndex/>
+      <AppIndex />
     </AuthProvider>
   );
 };
 
-export default App
+export default codePush(codePushOptions)(App);
